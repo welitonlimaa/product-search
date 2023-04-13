3c149d0b78ecb87dcbc72b15b9ceff876cbafb5a
welitonlimaa <welitonlima159@gmail.com>
2023-04-13 20:49:46 -0300

    refactor: server config with cors morgan

--- app/backend/src/index.js
+++ app/backend/src/index.js
@@ -1,26 +1,38 @@
 const express = require('express');
 require('dotenv').config()
+const cors = require('cors');
+const morgan = require('morgan');
+
 const router = require('./routers');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(
-  express.urlencoded({
-    extended: true,
-  }),
-);
-
-const accessControl = (_req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
-  res.header('Access-Control-Allow-Headers', '*');
-  next();
-};
+// app.use(
+//   express.urlencoded({
+//     extended: true,
+//   }),
+// );
+app.use(cors({
+  origin: '*'
+}));
 
 app.use(express.json());
-app.use(accessControl);
+
+app.use(morgan('tiny'));
+
+// const accessControl = (_req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
+//   res.header('Access-Control-Allow-Headers', '*');
+//   next();
+// };
+
+
+
+
+// app.use(accessControl);
 app.use('/', router);
 
 
