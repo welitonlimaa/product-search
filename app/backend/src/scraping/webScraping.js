0ce923ae05e490448260639e8c1ef3c66365a252
welitonlimaa <welitonlima159@gmail.com>
2023-04-13 15:00:31 -0300

    style: change loading animation

--- app/backend/src/scraping/webScraping.js
+++ app/backend/src/scraping/webScraping.js
@@ -3,9 +3,9 @@ const mlScraping = require("./mercadoLivre");
 
 const webScraping = async ({ searchFor, category, website }) => {
   const mercadoLivreProducts = await mlScraping({ searchFor, category, website })
-  console.log(mercadoLivreProducts);
+
   const buscapeProducts = await buscapeScraping({ searchFor, category, website });
-  console.log(buscapeProducts);
+
   const allProducts = [...mercadoLivreProducts, ...buscapeProducts];
 
   return allProducts;
