41cdbc2d6354d747ad38a9af19e296aeb706b39e
welitonlimaa <welitonlima159@gmail.com>
2023-04-12 22:15:32 -0300

    fetch: add button disabled for req empty

--- app/frontend/src/components/Loading.js
+++ app/frontend/src/components/Loading.js
@@ -1,8 +1,8 @@
 export default function Loading() {
   return (
     <div className="flex flex-wrap justify-around my-32">
-      <div class="flex items-center justify-center w-56 h-56">
-        <div class="px-16 py-4 text-lg font-black leading-none text-center text-blue-950 bg-blue-200 rounded-full animate-pulse dark:bg-blue-900 dark:text-blue-200">loading...</div>
+      <div className="flex items-center justify-center w-56 h-56">
+        <div className="px-16 py-4 text-lg font-black leading-none text-center text-blue-950 bg-blue-200 rounded-full animate-pulse dark:bg-blue-900 dark:text-blue-200">loading...</div>
       </div>
     </div>
   )
--- app/frontend/src/components/Searchbar.js
+++ app/frontend/src/components/Searchbar.js
@@ -1,15 +1,17 @@
 import Select from "./Select";
 
 export default function Seachbar({ searchData, setSearchData, getProducts }) {
+  const { searchFor, category, website } = searchData;
+
   return (
     <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
       <form className="flex flex-wrap w-full justify-around">
         <Select
           id="web"
           name="Web"
-          options={['Todos', 'Mercado Livre', 'Buscapé']}
+          options={['Todas', 'Mercado Livre', 'Buscapé']}
           setSearchData={(e) => setSearchData({ ...searchData, website: e.target.value })}
-          searchData={searchData.website}
+          searchData={website}
         />
 
         <Select
@@ -17,19 +19,21 @@ export default function Seachbar({ searchData, setSearchData, getProducts }) {
           name="Categorías"
           options={['Geladeira', 'TV', 'Celular']}
           setSearchData={(e) => setSearchData({ ...searchData, category: e.target.value })}
-          searchData={searchData.category}
+          searchData={category}
         />
 
         <input
           type="search"
           id="search"
           onChange={(e) => setSearchData({ ...searchData, searchFor: e.target.value })}
-          value={searchData.searchFor}
-          className="w-96 p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..." required
+          value={searchFor}
+          className="w-96 p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..."
+          required
         />
 
         <button
           type="button"
+          disabled={!(searchFor && category && website)}
           onClick={() => getProducts()}
           className="w-24 -ms-12 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-4 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
           Search
