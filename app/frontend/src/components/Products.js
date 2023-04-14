750d1ee265d5fb2dd21d1477d31417644aa3d7ba
welitonlimaa <welitonlima159@gmail.com>
2023-04-13 23:02:54 -0300

    fix: scraping test performance railway

--- app/frontend/src/components/Products.js
+++ app/frontend/src/components/Products.js
@@ -4,7 +4,7 @@ export default function Products({ productsData }) {
   return (
     <div className="flex flex-wrap justify-around my-32">
       {
-        productsData.map((product, index) => <ProductCard key={index} productData={product} />)
+        productsData.message === undefined ? productsData.map((product, index) => <ProductCard key={index} productData={product} />) : productsData.message
       }
     </div>
   )
