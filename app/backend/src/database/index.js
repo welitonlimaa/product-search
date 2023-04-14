5b2bd3186491a63a4fbdfa801d6a1c23c425ed73
welitonlimaa <welitonlima159@gmail.com>
2023-04-14 18:09:58 -0300

    fix: db connection

--- app/backend/src/database/index.js
+++ app/backend/src/database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const MONGO_DB_URL = process.env.MONGO_ddSTRING || 'mongodb://localhost:8081/productsearch';
+const MONGO_DB_URL = process.env.MONGO_STRING || 'mongodb://localhost:8081/productsearch';
 
 mongoose.connect(MONGO_DB_URL);
 
