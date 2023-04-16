4d65ba7e875c22cab3f8d92598aa47341649205f
welitonlimaa <welitonlima159@gmail.com>
2023-04-15 21:04:32 -0300

    style: change animation loading component

--- app/frontend/src/components/Loading.js
+++ app/frontend/src/components/Loading.js
@@ -1,14 +1,9 @@
 export default function Loading() {
   return (
-    <div className="loop-wrapper">
-      <div className="mountain"></div>
-      <div className="hill"></div>
-      <div className="tree"></div>
-      <div className="tree"></div>
-      <div className="tree"></div>
-      <div className="rock"></div>
-      <div className="truck"></div>
-      <div className="wheels"></div>
+    <div className="flex flex-wrap justify-around my-32">
+      <div className="flex items-center justify-center w-56 h-56">
+        <div className="px-16 py-4 text-lg font-black leading-none text-center text-blue-950 bg-blue-200 rounded-full animate-pulse dark:bg-blue-900 dark:text-blue-200">loading...</div>
+      </div>
     </div>
   )
 }
