f1aa9f09caed2c71a5ae70389dc61d79b913756e
welitonlimaa <welitonlima159@gmail.com>
2023-04-27 16:54:06 -0300

    style: add resposiveness

--- app/frontend/src/components/Select.js
+++ app/frontend/src/components/Select.js
@@ -4,7 +4,7 @@ export default function Select({ id, name, options, searchData, setSearchData })
       id={id}
       onChange={setSearchData}
       defaultValue={searchData}
-      className="bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+      className="min-[641px]:p-2.5 max-[640px]:w-full max-[640px]:p-4 max-[640px]:m-1 bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-640 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
       <option value="">{name}</option>
       {
         options.map((option, index) => <option key={index} value={option}>{option}</option>)
